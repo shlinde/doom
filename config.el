f18022f6cb4d58f7c76f88307d04aa5518826fa4
Sebastian Hempel Linde <sebastian@hempellinde.com>
2025-04-25 08:24:58 +0200

    feat: update

--- config.el
+++ config.el
@@ -41,6 +41,11 @@
   :commands gptel gptel-menu gptel-mode gptel-send
   :config
   (gptel-make-gemini "Gemini" :key (getenv "GEMINI_API_KEY") :stream t)
+  (setq
+   gptel-model 'gemini-2.0-flash-thinking-exp-01-21
+   gptel-backend (gptel-make-gemini "Gemini"
+                   :key (getenv "GEMINI_API_KEY")
+                   :stream t))
   (setq gptel-default-mode #'org-mode))
 
 (use-package! lsp-pyright
